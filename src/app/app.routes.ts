8cb386e100cdb125985e063c2ef2f31d63de4d50
loctetgenius <sagnamoussa546@gmail.com>
2024-08-01 14:23:29 +0000

    fixe bugg concernant la route par défaut

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { IndexComponent } from './index/index.component';
 import { ViewComponent } from './view/view.component';
 
 export const routes: Routes = [
-  {path:'post', redirectTo:'index', pathMatch:'full'},
+  {path:'', redirectTo:'index', pathMatch:'full'},
   {path:'index', component:IndexComponent},
   {path:'create', component:CreateComponent},
   {path:':postId/edit', component:EditComponent},
